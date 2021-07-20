edc107736fe8e67538ffaad62f0994f818404cbc
arifkhan10 <barfikhan78621@gmail.com>
2021-07-20 19:29:33 +0530

    work done

---
+++ Employee.py
@@ -0,0 +1,13 @@
+employeename=["Arif","Siddharth","Gulshan","Amarthiga"]
+employeeid=[101,102,103,104]
+print(employeename+employeeid)
+print(employeename)
+del employeename[2]
+print(employeename)
+employeename.remove("Arif")
+employeename.pop(0)
+print(employeename)
+employeename.clear()
+print(employeename)
+employeeid.clear()
+print(employeeid)
---
+++ movie.py
@@ -0,0 +1,9 @@
+moviename=["PK","My name is Khan","Mumbai saga","Sultan"]
+print(moviename)
+del moviename[2]
+print(moviename)
+moviename.remove("PK")
+moviename.pop(0)
+print(moviename)
+moviename.clear()
+print(moviename)
